53f0661eb2c4c93e8722f3bfa2c7492098a766d1
zjkal <javachina@foxmail.com>
2023-09-01 15:53:19 +0800

    Initial commit

---
+++ src/MysqlHelper.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types=1);
+
+namespace zjkal;
+
+use Exception;
+use InvalidArgumentException;
+
+/**
+ * 最方便的mysql操作类,可以便捷导入.sql文件和将数据库导出为.sql文件
+ * Class MysqlHelper
+ * @package zjkal
+ */
+class MysqlHelper
+{
+
+}
