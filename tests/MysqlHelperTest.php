b9da98f8d92bda729d67b4dccaaeaefbc6b17a26
zjkal <javachina@foxmail.com>
2023-09-23 12:57:05 +0800

    修改测试用例

--- tests/MysqlHelperTest.php
+++ tests/MysqlHelperTest.php
@@ -9,17 +9,31 @@ class MysqlHelperTest extends TestCase
 {
     private $config = ['username' => 'root', 'password' => 'root', 'database' => 'testdatabase'];
 
-    public function testExportSqlFile()
-    {
-        $mysql = new MysqlHelper();
-        $mysql->setConfig($this->config);
-        $mysql->exportSqlFile('test.sql');
-    }
-
     public function testImportSqlFile()
     {
-        $mysql = new MysqlHelper();
-        $mysql->setConfig($this->config);
-        $mysql->importSqlFile('test.sql', 'test_');
+        $this->expectOutputString('导入成功');
+
+        try {
+            $mysql = new MysqlHelper();
+            $mysql->setConfig($this->config);
+            $mysql->importSqlFile('import.sql', 'test_');
+            print '导入成功';
+        } catch (\Exception $e) {
+            $this->fail('导入失败:' . $e->getMessage());
+        }
+    }
+
+    public function testExportSqlFile()
+    {
+        $this->expectOutputString('导出成功');
+
+        try {
+            $mysql = new MysqlHelper();
+            $mysql->setConfig($this->config);
+            $mysql->exportSqlFile('export.sql');
+            print '导出成功';
+        } catch (\Exception $e) {
+            $this->fail('导出失败:' . $e->getMessage());
+        }
     }
 }
